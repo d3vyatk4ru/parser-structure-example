29e7bc080ea09251c439378b4f50c38849ff252e
d3vyatk4ru <danya.devyatkin@mail.ru>
2023-10-25 23:32:26 +0300

    add func for getting data fom json

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -9,13 +10,6 @@ import (
 )
 
 const APIKEY = "a6cc01f8b7350c685d5afe22c3b63a04"
-
-type Weather struct {
-	Country     string  `json:"country"`
-	Area        string  `json:"area"`
-	Temperature float32 `json:"temperature"`
-	Description string  `json:"desc"`
-}
 
 func CreateURL(
 	domain string,
@@ -28,7 +22,7 @@ func CreateURL(
 	params := url.Values{}
 	params.Add("lat", fmt.Sprintf("%v", lat))
 	params.Add("lon", fmt.Sprintf("%v", lon))
-	params.Add("appid", apikey)
+	params.Add("APPID", apikey)
 
 	u, err := url.ParseRequestURI(domain)
 
@@ -39,40 +33,67 @@ func CreateURL(
 	u.Path = resource
 	u.RawQuery = params.Encode()
 
+	log.Println(u.String())
+
 	return u.String(), nil
 }
 
-func RequestToWeather(URL string) error {
+func RequestToWeather(URL string) ([]byte, error) {
 
 	resp, err := http.Get(URL)
 
 	if err != nil {
-		return err
+		return []byte{}, err
 	}
 
-	resp.Body.Close()
+	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 
 	if err != nil {
-		return err
+		return []byte{}, err
 	}
 
-	fmt.Println(body)
+	return body, nil
+}
 
-	return nil
+func UnmarshalWeatherJSON(body []byte) (NeccesaryWeather, error) {
 
+	jsonData := JsonStructureParse{}
+
+	err := json.Unmarshal(body, &jsonData)
+
+	if err != nil {
+		return NeccesaryWeather{}, err
+	}
+
+	return NeccesaryWeather{
+		Country:     jsonData.Sys.Country,
+		Name:        jsonData.Name,
+		Temperature: jsonData.Main.Temperature,
+		Description: jsonData.Weather[0].Description,
+	}, nil
 }
 
 func main() {
 
-	URL, err := CreateURL("https://openweathermap.org", "/data/2.5/weather", 10, 5, APIKEY)
-
-	fmt.Println(URL)
+	URL, err := CreateURL("https://api.openweathermap.org", "/data/2.5/weather", 10, 5, APIKEY)
 
 	if err != nil {
 		log.Printf("Error: %s\n", err.Error())
 	}
 
-	RequestToWeather(URL)
+	body, err := RequestToWeather(URL)
+
+	if err != nil {
+		log.Printf("Error: %s\n", err.Error())
+	}
+
+	data, err := UnmarshalWeatherJSON(body)
+
+	if err != nil {
+		log.Printf("Error: %s\n", err.Error())
+	}
+
+	log.Println(data)
 }
